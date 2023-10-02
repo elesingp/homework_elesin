10063862c6d0faaa6a112d59d19eb192130bca08
elesingp <105668974+elesingp@users.noreply.github.com>
2023-10-03 02:58:35 +0300

    Update my_k_nearest_neighbor.py

--- my_k_nearest_neighbor.py
+++ my_k_nearest_neighbor.py
@@ -12,15 +12,12 @@ class KNearestNeighbor:
         self.y_train = y # numpy array shape (num_train,)
       
     def compute_distances_two_loops(self, X):
-
-        num_train = self.X_train.shape[0]
-        num_test = X.shape[0] 
-        dists = np.zeros((num_test, num_train))
-        for i in range(num_test):
-            for j in range(num_train):
-                dists[i][j] = math.sqrt((X_test[i])**2 + (X_train[j])**2)
-
-        return dists     
-
+      num_train = self.X_train.shape[0]
+      num_test = X.shape[0]
+      dists = np.zeros((num_test, num_train))
+      for i in range(num_test):
+          for j in range(num_train):
+            dists[i][j] = math.sqrt(np.sum((X[i] - self.X_train[j])**2))
+      return dists
 
 

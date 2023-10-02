79f56a69bb2c78d8ecf4a4d53fd94147fbb6047f
elesingp <105668974+elesingp@users.noreply.github.com>
2023-10-03 01:36:05 +0300

    Update my_k_nearest_neighbor.py

--- my_k_nearest_neighbor.py
+++ my_k_nearest_neighbor.py
@@ -1,25 +1,12 @@
-import numpy as np
-"""
-Credits: the original code belongs to Stanford CS231n course assignment1. Source link: http://cs231n.github.io/assignments2019/assignment1/
-"""
+# import numpy as np
 
 class KNearestNeighbor:
-    """ a kNN classifier with L2 distance """
 
     def __init__(self):
         pass
 
     def fit(self, X, y):
-        """
-        Train the classifier. For k-nearest neighbors this is just
-        memorizing the training data.
 
-        Inputs:
-        - X: A numpy array of shape (num_train, D) containing the training data
-          consisting of num_train samples each of dimension D.
-        - y: A numpy array of shape (N,) containing the training labels, where
-             y[i] is the label for X[i].
-        """
         self.X_train = X
         self.y_train = y
 

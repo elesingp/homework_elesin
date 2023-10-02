26c5774bb1d1e5fa3e42a713d80f90b3becf78b5
elesingp <105668974+elesingp@users.noreply.github.com>
2023-10-02 23:43:54 +0300

    Add files via upload

---
+++ my_k_nearest_neighbor.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class KNearestNeighbor:
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+
+
+
+
+
+
+
